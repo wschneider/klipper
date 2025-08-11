11d0033f1a5ce0cd20a2e8bcacfc7cc8db0f074a
wschneider <willschneider.513@gmail.com>
2025-08-11 17:57:19 -0400

    Add logging on speed reductions

--- klippy/kinematics/polar.py
+++ klippy/kinematics/polar.py
@@ -150,6 +150,13 @@ class PolarKinematics:
                 max_linear_accel = min_r * self.max_r_accel
                 
                 # Apply the limit if it's more restrictive than current limits
+                logging.info("Moving from (%f, %f) to (%f, %f) with angle change %f radians",
+                             move.start_pos[0], move.start_pos[1],
+                             end_pos[0], end_pos[1], angle_diff)
+                logging.info("Adjusting velocity and acceleration from: %f, %f to: %f, %f",
+                             move.max_velocity, move.max_accel,
+                             max_linear_velocity, max_linear_accel)
+
                 move.limit_speed(max_linear_velocity, max_linear_accel)
 
 
